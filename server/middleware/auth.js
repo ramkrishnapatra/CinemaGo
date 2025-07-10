1e30a0130103332a0033901ee744e09ba8f13410
ramkrishnapatra <ramkrishnapatra136@gmail.com>
2025-07-10 09:03:03 +0530

    connect client and server

---
+++ server/middleware/auth.js
@@ -0,0 +1,21 @@
+import {clerkClient} from "@clerk/express"
+
+export const protectAdmin=async(req,res,next)=>{
+    try {
+        const {userId}=req.auth();
+
+        const user=await clerkClient.getUser(userId)
+
+        if(user.privateMetadata.role !== 'admin'){
+            return res.json({
+                success:false,message:"not authorized"
+            })
+        }
+
+        next();
+    } catch (error) {
+      return res.json({
+                success:false,message:"not authorized"
+            }) 
+    }
+}
--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
+import showRouter from './routes/showRoutes.js';
+import bookingRouter from './routes/bookingRoutes.js';
+import adminRouter from './routes/adminRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 const app=express();
 const port=3000;
@@ -20,7 +24,10 @@ app.use(clerkMiddleware())
 // API routes
 app.get('/',(req,res)=>res.send('server is live!!'))
 app.use('/api/inngest',serve({ client: inngest, functions }))
-
+app.use('/api/show',showRouter)
+app.use('/api/booking',bookingRouter)
+app.use('/api/admin',adminRouter)
+app.use('/api/user',userRouter)
 
 
 app.listen(port,()=>console.log(`server listening at http://localhost:${port}`))
