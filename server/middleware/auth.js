0f3ef299e555ca645803a610d95b3926bae8a303
ramkrishnapatra <ramkrishnapatra136@gmail.com>
2025-07-12 08:12:48 +0530

    auth testing

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,54 +1,21 @@
-// import {clerkClient} from "@clerk/express"
+import {clerkClient} from "@clerk/express"
 
-// export const protectAdmin=async(req,res,next)=>{
-//     try {
-//         const {userId}=req.auth();
+export const protectAdmin=async(req,res,next)=>{
+    try {
+        const {userId}=req.auth();
 
-//         const user=await clerkClient.users.getUser(userId)
+        const user=await clerkClient.users.getUser(userId)
 
-//         if(user.privateMetadata.role !== 'admin'){
-//             return res.json({
-//                 success:false,message:"not authorized"
-//             })
-//         }
+        if(user.privateMetadata.role !== 'admin'){
+            return res.json({
+                success:false,message:"not authorized"
+            })
+        }
 
-//         next();
-//     } catch (error) {
-//       return res.json({
-//                 success:false,message:"not authorized"
-//             }) 
-//     }
-// }
-
-
-import { clerkClient } from "@clerk/express";
-
-export const protectAdmin = async (req, res, next) => {
-  try {
-    const { userId } = req.auth();
-
-    if (!userId) {
-      return res.status(401).json({
-        success: false,
-        message: "Unauthorized: No user ID found",
-      });
+        next();
+    } catch (error) {
+      return res.json({
+                success:false,message:"not authorized"
+            }) 
     }
-
-    const user = await clerkClient.users.getUser(userId);
-
-    if (user?.privateMetadata?.role !== "admin") {
-      return res.status(403).json({
-        success: false,
-        message: "Forbidden: Not authorized as admin",
-      });
-    }
-
-    next();
-  } catch (error) {
-    console.error("protectAdmin error:", error);
-    return res.status(500).json({
-      success: false,
-      message: "Internal Server Error",
-    });
-  }
-};
+}
